57e02cf2efb0d1ad2b32097c8fadb54cdc6f897d
Paul Boekschoten <paul.boekschoten@hashicorp.com>
2022-06-27 13:49:43 +0200

    subnets added

--- network.tf
+++ network.tf
@@ -5,3 +5,34 @@ resource "aws_vpc" "main" {
     Name = var.name_tag
   }
 }
+
+# subnets
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_private
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Name = "${var.name_tag}-private"
+  }
+}
+
+resource "aws_subnet" "public1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_public1
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Name = "${var.name_tag}-public1"
+  }
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_public2
+  availability_zone = "${var.region}b"
+
+  tags = {
+    Name = "${var.name_tag}-public2"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,17 +1,39 @@
+# used for naming and tagging resources
 variable "name_tag" {
   description = "global name for resource tags"
   type        = string
   default     = "paul-skillsmap-10-tf"
 }
 
+# region
 variable "region" {
   description = "AWS region"
   type        = string
   default     = "eu-west-3"
 }
 
+# vpc cidr block
 variable "vpc_cidr" {
   description = "CIDR of the VPC"
   type        = string
   default     = "10.1.0.0/16"
+}
+
+# subnet cidr blocks
+variable "subnet_private" {
+  description = "private subnet cidr"
+  type        = string
+  default     = "10.1.0.0/24"
+}
+
+variable "subnet_public1" {
+  description = "public1 subnet cidr"
+  type        = string
+  default     = "10.1.1.0/24"
+}
+
+variable "subnet_public2" {
+  description = "public2 subnet cidr"
+  type        = string
+  default     = "10.1.2.0/24"
 }
