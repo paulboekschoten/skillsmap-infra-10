ec7d03a0d7f35d5fc8261a1a1f712d6d7ca8e140
Paul Boekschoten <paul.boekschoten@hashicorp.com>
2022-06-27 14:01:53 +0200

    NAT added

--- gateways.tf
+++ gateways.tf
@@ -6,3 +6,25 @@ resource "aws_internet_gateway" "gw" {
     Name = var.name_tag
   }
 }
+
+# elastic ip for NAT gateway
+resource "aws_eip" "nat" {
+  vpc = true
+  tags = {
+    Name = var.name_tag
+  }
+}
+
+# NAT gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public1.id
+
+  tags = {
+    Name = var.name_tag
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
+}
